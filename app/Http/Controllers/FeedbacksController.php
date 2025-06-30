5f1609c76fc3c89c705241995a9d76984773a139
kairui1012 <TP074684@mail.apu.edu.my>
2025-06-30 09:14:06 +0800

    修复完成

--- app/Http/Controllers/FeedbacksController.php
+++ app/Http/Controllers/FeedbacksController.php
@@ -13,7 +13,7 @@ class FeedbacksController extends Controller
         $week = now()->format('o-\WW');
         return Feedback::firstOrCreate(
             ['phone' => $userPhone, 'week' => $week],
-            ['good' => null, 'bad' => null, 'remark' => null , 'reference' => null]
+            ['good' => null, 'bad' => null, 'remark' => null , 'referrer' => null]
         );
     }
 
@@ -24,7 +24,7 @@ class FeedbacksController extends Controller
             'good' => 'required|string',
             'bad' => 'required|string',
             'remark' => 'nullable|string',
-            'reference' => 'nullable|string',
+            'referrer' => 'nullable|string',
         ]);
 
         $userPhone = Auth::user()->phone;
@@ -33,7 +33,7 @@ class FeedbacksController extends Controller
         $feedback->good = $request->input('good');
         $feedback->bad = $request->input('bad');
         $feedback->remark = $request->input('remark');
-        $feedback->reference = $request->input('reference');
+        $feedback->referrer = $request->input('referrer');
         $feedback->save();
 
         return redirect()->back()->with('success', '反馈已保存');
